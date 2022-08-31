2e20855125182d6fa4a3d7422be8914a3d00d0f7
asimqasmi <kaabteacher@gmail.com>
2022-08-31 15:52:28 +0400

    change config site

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
-const dev = process.env.NODE_ENV !== 'production'
+const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
-  ? 'http://localhost:3000'
-  : 'https://lessons22.vercel.app/'
+  ? "http://localhost:3000"
+  : "https://lsnk23.vercel.app/";
